4766953aa20a0de0595db40d8d6d3fd2400bd086
吉渕 直道 <yoshibuchi.n@gmail.com>
2023-01-05 00:42:51 +0900

    [add]log4net追加
    一旦、実装。
    以後、どのように利用するか。検討する
    参考URL
    https://itsakura.com/csharp-log4net

--- TutorialMoneyAdmin/TutorialMoneyAdmin/Controllers/HomeController.cs
+++ TutorialMoneyAdmin/TutorialMoneyAdmin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace TutorialMoneyAdmin.Controllers
 {
@@ -10,6 +12,13 @@ namespace TutorialMoneyAdmin.Controllers
     {
         public ActionResult Index()
         {
+            ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            logger.Debug("開発中のデバッグ／トレースに使用する");
+            logger.Info("情報（操作履歴等）");
+            logger.Warn("注意／警告（障害の一歩手前）");
+            logger.Error("システムが停止するまではいかない障害が発生");
+            logger.Fatal("システムが停止する致命的な障害が発生");
             return View();
         }
 
